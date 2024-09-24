4d40cef2501d2acc9643c31c23525ab01ddbbfba
shivam <shivamkesharwani6767@gmail.com>
2024-09-25 00:50:25 +0530

    app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/entities/todo.entity';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo.db',
+      entities: [Todo],
+      synchronize: true, // Automatically sync the database schema (for development only)
+    }),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
